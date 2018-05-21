abaad86751b4d553dfb65e0dffe60d739c6f88f7
nhh <Niklas.Hanft@outlook.com>
2018-05-21 14:03:23 +0200

    Add example job

---
+++ src/main/java/com/mediheroes/mediheroes/job/SessionFlushJob.java
@@ -0,0 +1,22 @@
+package com.mediheroes.mediheroes.job;
+
+import io.lettuce.core.RedisClient;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class SessionFlushJob {
+
+    private final RedisClient redisClient;
+
+    public SessionFlushJob(
+        RedisClient redisClient
+    ){
+        this.redisClient = redisClient;
+    }
+
+    // Todo implement jobs like this:
+    //@Scheduled(cron = cronTimer)
+    private void clearSessionStorage(){
+        redisClient.connect().async().flushdb();
+    }
+
+}
