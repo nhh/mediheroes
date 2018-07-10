fc3039a9e954363e9be92439f6e2ce2b56f103f4
nhh <Niklas.Hanft@outlook.com>
2018-07-10 06:23:55 +0200

    Move file actions into its own controller

---
+++ src/main/java/com/mediheroes/mediheroes/controller/api/v1/user/FilesController.java
@@ -0,0 +1,85 @@
+package com.mediheroes.mediheroes.controller.api.v1.user;
+
+import com.mediheroes.mediheroes.dto.DocumentResponse;
+import com.mediheroes.mediheroes.exception.EntityNotFoundException;
+import com.mediheroes.mediheroes.exception.FileNotFoundException;
+import com.mediheroes.mediheroes.service.UserService;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.MimeType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequestMapping("/api/v1/users")
+@RestController
+@Transactional
+public class FilesController {
+
+    private final UserService userService;
+
+    public FilesController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/{id}/files")
+    public ResponseEntity uploadFile(
+        @PathVariable Long id,
+        @RequestBody MultipartFile file
+    ){
+        var user = userService
+            .getCurrentUser()
+            .orElseThrow(EntityNotFoundException::new);
+
+        userService.addFile(user, file, user);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}/files")
+    public ResponseEntity<DocumentResponse[]> getDocuments(@PathVariable Long id){
+        var sender = userService
+            .getCurrentUser()
+            .orElseThrow(EntityNotFoundException::new);
+
+        var user = userService
+            .find(id)
+            .orElseThrow(EntityNotFoundException::new);
+
+        var response = userService.getFiles(user, sender)
+            .stream()
+            .map(DocumentResponse::new)
+            .toArray(DocumentResponse[]::new);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/files/{id}")
+    public ResponseEntity getUploadedFile(
+        @PathVariable Long userId,
+        @PathVariable String id
+    ){
+        var user = userService
+            .getCurrentUser()
+            .orElseThrow(EntityNotFoundException::new);
+
+        var resource = userService
+            .getUploadedFileResource(id)
+            .orElseThrow(FileNotFoundException::new);
+
+        try {
+            return ResponseEntity
+                .ok()
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.asMediaType(MimeType.valueOf(resource.getContentType())))
+                .body(new InputStreamResource(resource.getInputStream()));
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
--- src/main/java/com/mediheroes/mediheroes/controller/api/v1/user/UserController.java
+++ src/main/java/com/mediheroes/mediheroes/controller/api/v1/user/UserController.java
@@ -2,22 +2,19 @@ package com.mediheroes.mediheroes.controller.api.v1.user;
 
 import com.mediheroes.mediheroes.domain.user.Address;
 import com.mediheroes.mediheroes.domain.user.User;
-import com.mediheroes.mediheroes.dto.DocumentResponse;
-import com.mediheroes.mediheroes.dto.user.*;
+import com.mediheroes.mediheroes.dto.user.UserAddressRequest;
+import com.mediheroes.mediheroes.dto.user.UserProfileRequest;
+import com.mediheroes.mediheroes.dto.user.UserRequest;
+import com.mediheroes.mediheroes.dto.user.UserResponse;
 import com.mediheroes.mediheroes.exception.EntityNotFoundException;
-import com.mediheroes.mediheroes.exception.FileNotFoundException;
 import com.mediheroes.mediheroes.service.UserService;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.IOException;
 import java.util.stream.StreamSupport;
 
 @RequestMapping("/api/v1/users")
@@ -138,64 +135,6 @@ public class UserController {
         var fileId = userService.updateProfileImage(user, file, user);
 
         return new ResponseEntity<>(fileId, HttpStatus.OK);
-    }
-
-    @GetMapping("/{userId}/files/{id}")
-    public ResponseEntity getUploadedFile(
-        @PathVariable Long userId,
-        @PathVariable String id
-    ){
-        var user = userService
-            .getCurrentUser()
-            .orElseThrow(EntityNotFoundException::new);
-
-        var resource = userService
-            .getUploadedFileResource(id)
-            .orElseThrow(FileNotFoundException::new);
-
-        try {
-            return ResponseEntity
-                .ok()
-                .contentLength(resource.contentLength())
-                .contentType(MediaType.asMediaType(MimeType.valueOf(resource.getContentType())))
-                .body(new InputStreamResource(resource.getInputStream()));
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @GetMapping("/{id}/files")
-    @Transactional
-    public ResponseEntity<DocumentResponse[]> getDocuments(@PathVariable Long id){
-        var sender = userService
-            .getCurrentUser()
-            .orElseThrow(EntityNotFoundException::new);
-
-        var user = userService
-            .find(id)
-            .orElseThrow(EntityNotFoundException::new);
-
-        var response = userService.getFiles(user, sender)
-            .stream()
-            .map(DocumentResponse::new)
-            .toArray(DocumentResponse[]::new);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @PostMapping("/{id}/files")
-    public ResponseEntity uploadFile(
-        @PathVariable Long id,
-        @RequestBody MultipartFile file
-    ){
-        var user = userService
-            .getCurrentUser()
-            .orElseThrow(EntityNotFoundException::new);
-
-        userService.addFile(user, file, user);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
